d19d3d374f4d780ce66d95354086029a3d49477e
gnos28 <gnos28@gmail.com>
2022-03-03 18:36:08 +0100

    handleClick & handleChange

--- src/App.js
+++ src/App.js
@@ -1,29 +1,62 @@
+import React from "react";
+import { render } from "react-dom";
 import "./App.css";
 import Result from "./Result";
 
-function App() {
-  return (
-    <div className="container">
-      <h1>CSS file analyzer using caniuse API</h1>
-      <form>
-        <div class="form-group">
-          <label for="exampleFormControlTextarea1">
-            Paste your css file here
-          </label>
-          <textarea
-            className="form-control"
-            id="exampleFormControlTextarea1"
-            rows="3"
-            placeholder="Paste your css file here"
-          ></textarea>
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </div>
-      </form>
-      <Result />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      textarea: "",
+      dataToSubmit: "",
+    };
+  }
+
+  handleClick = (e) => {
+    e.preventDefault();
+    this.setState({
+      dataToSubmit: this.state.textarea
+    }, () => console.log(this.state));
+    
+  };
+
+  handleChange = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      textarea: e.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h1>CSS file analyzer using caniuse API</h1>
+        <form>
+          <div className="form-group">
+            <label htmlFor="exampleFormControlTextarea1">
+              Paste your css file here
+            </label>
+            <textarea
+              className="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"
+              placeholder="Paste your css file here"
+              value={this.state.textarea}
+              onChange={(e) => this.handleChange(e)}
+            ></textarea>
+            <button
+              type="submit"
+              className="btn btn-primary"
+              onClick={(e) => this.handleClick(e)}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+        <Result data={this.state.dataToSubmit} />
+      </div>
+    );
+  }
 }
 
 export default App;
